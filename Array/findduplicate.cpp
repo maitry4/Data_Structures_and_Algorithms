bcfccae1cd9f9c705723db13a7cf805392564460
Maitry Parikh <maitryparikh23@gmail.com>
2023-10-28 16:43:00 +0530

    I solved two array questions today.
    unique no of occurrences was a difficult question for me. I haven't used maps to solve it just a simple brute force approach.

---
+++ Array/findduplicate.cpp
@@ -0,0 +1,21 @@
+// https://www.codingninjas.com/studio/problems/duplicate-in-array_893397?
+#include<iostream>
+#include<vector>
+using namespace std;
+int finddup(vector<int> arr, int n) {
+    int ans = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        ans ^= arr[i];
+        if((i+1)!=arr.size())
+        {
+            ans^=(i+1);
+        }
+    }
+    return ans;
+}
+int main() {
+    vector<int> a{1,2,3,4,3,5};
+    cout<<finddup(a, 6);
+    return 0;
+}
