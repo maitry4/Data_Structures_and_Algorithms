b7cc634adb15754590ff4079a29363c224d94d9c
Maitry Parikh <maitryparikh23@gmail.com>
2023-11-01 18:38:18 +0530

    solved 1 question

---
+++ Array/sort01.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+void sortZeroesAndOne(int *input, int size)
+{
+    int i=0,j=size-1;
+    while(i<=j)
+    {
+        while(input[i]==0 && i<=j)
+        {
+            i++;
+        }
+        while(input[j]==1 && i<=j)
+        {
+            j--;
+        }
+        if(i<=j)
+        {
+            swap(input[i],input[j]);
+            j--;
+            i++;
+        }
+    }
+}
+int main()
+{
+    int arr[7] = {0,1,1,0,1,0,1};
+    sortZeroesAndOne(arr,7);
+    for(int i=0; i<7; i++)
+    {
+        cout<<arr[i];
+    }
+    return 0;
+}
